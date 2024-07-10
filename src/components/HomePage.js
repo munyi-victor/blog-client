7d46a09f27b25f6cdbb2389aae04f375cd76cf34
Victor Munyi <munyivictor6@gmail.com>
2024-07-10 10:49:59 +0300

    blig date

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -27,25 +27,28 @@ const HomePage = () => {
     getBlogs();
   }, [loggedInUserId, login]);
 
-  // Function to calculate elapsed time
-  const calculateElapsedTime = (date) => {
-    const now = new Date();
-    const diff = now.getTime() - date.getTime();
-    const seconds = Math.floor(diff / 1000);
+// Function to calculate elapsed time
+const calculateElapsedTime = (date) => {
+  const now = new Date();
+  const blogDate = new Date(date)
+  const diff = now.getTime() - blogDate.getTime();
 
-    if (seconds < 60) {
-      return "Just now";
-    } else if (seconds < 3600) {
-      const minutes = Math.floor(seconds / 60);
-      return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
-    } else if (seconds < 86400) {
-      const hours = Math.floor(seconds / 3600);
-      return `${hours} hour${hours > 1 ? "s" : ""} ago`;
-    } else {
-      const days = Math.floor(seconds / 86400);
-      return `${days} day${days > 1 ? "s" : ""} ago`;
-    }
-  };
+  // Convert milliseconds difference to seconds
+  const seconds = Math.floor(diff / 1000);
+
+  if (seconds < 60) {
+    return 'Just now';
+  } else if (seconds < 3600) {
+    const minutes = Math.floor(seconds / 60);
+    return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+  } else if (seconds < 86400) {
+    const hours = Math.floor(seconds / 3600);
+    return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+  } else {
+    const days = Math.floor(seconds / 86400);
+    return `${days} day${days > 1 ? 's' : ''} ago`;
+  }
+};
 
   return (
     <div className="container text-center p-2 w-75">
