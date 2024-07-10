c856d779f5ffff5ae327e1588155645ebba03e1f
Victor Munyi <munyivictor6@gmail.com>
2024-07-10 09:55:42 +0300

    modified

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -27,9 +27,37 @@ const HomePage = () => {
     getBlogs();
   }, [loggedInUserId, login]);
 
-  if (blogs === null) {
-    return <div>Loading...</div>;
-  }
+  // if (blogs === null) {
+  //   return <div>Loading...</div>;
+  // }
+
+  const [dateFromMySQL, setDateFromMySQL]  = useState("")
+
+  blogs.forEach (blog => {
+    setDateFromMySQL(blog.date)
+  })
+
+  const date = new Date(dateFromMySQL);
+
+  // Function to calculate elapsed time
+  const calculateElapsedTime = (date) => {
+    const now = new Date();
+    const diff = now.getTime() - date.getTime();
+    const seconds = Math.floor(diff / 1000);
+
+    if (seconds < 60) {
+      return 'Just now';
+    } else if (seconds < 3600) {
+      const minutes = Math.floor(seconds / 60);
+      return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+    } else if (seconds < 86400) {
+      const hours = Math.floor(seconds / 3600);
+      return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } else {
+      const days = Math.floor(seconds / 86400);
+      return `${days} day${days > 1 ? 's' : ''} ago`;
+    }
+  };
 
   return (
     <div className="container text-center p-2 w-75">
@@ -57,7 +85,7 @@ const HomePage = () => {
             <h5>{blog.title}</h5>
             <p>{blog.body}</p>
             <p>
-              <b>{blog.date}</b>
+              <b>{calculateElapsedTime(blog.date)}</b>
             </p>
           </div>
         ))}
